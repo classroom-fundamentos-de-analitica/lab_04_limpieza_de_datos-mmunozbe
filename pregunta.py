bd3bdfe6a2b2c763fdea2b9db6b58fb545ba56ba
Matheo Muñoz Betancur <131404918+mmunozbe@users.noreply.github.com>
2024-03-08 11:25:09 -0500

    :bento:

--- pregunta.py
+++ pregunta.py
@@ -12,9 +12,16 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+    df["idea_negocio"] = df["idea_negocio"].str.lower().str.strip(" ")
+    df["barrio"] = df["barrio"].str.lower().str.strip(" ")
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int).astype(str)
+    df["monto_del_credito"] = df["monto_del_credito"].str.strip(" ").str.strip("$").str.replace(",", "").str.replace(".", "")
+    df["línea_credito"] = df["línea_credito"].str.lower().str.strip().str.replace("-", " ").str.replace("_", " ").str.strip(" ")
+    
+    
     return df
+
+df = clean_data()
+print(df.línea_credito.value_counts().to_list())
