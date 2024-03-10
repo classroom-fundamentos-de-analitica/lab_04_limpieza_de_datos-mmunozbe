06a4bfbd209f2a3382efa100ef99ae96a456a79e
Matheo Muñoz Betancur <131404918+mmunozbe@users.noreply.github.com>
2024-03-10 12:21:09 -0500

    :bento:

--- pregunta.py
+++ pregunta.py
@@ -6,22 +6,38 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+
 import pandas as pd
 
 
 def clean_data():
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df["sexo"] = df["sexo"].str.lower()
-    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
-    df["idea_negocio"] = df["idea_negocio"].str.lower().str.strip(" ")
-    df["barrio"] = df["barrio"].str.lower().str.strip(" ")
-    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int).astype(str)
-    df["monto_del_credito"] = df["monto_del_credito"].str.strip(" ").str.strip("$").str.replace(",", "").str.replace(".", "")
-    df["línea_credito"] = df["línea_credito"].str.lower().str.strip().str.replace("-", " ").str.replace("_", " ").str.strip(" ")
-    
-    
+    df = df.replace("-", " ", regex=True).replace("_", " ", regex=True)
+    df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+
+    # Limpieza de monto_del_credito
+    df["monto_del_credito"] = (
+        df["monto_del_credito"]
+        .str.strip(" ")
+        .str.replace("[,$]|(\.00$)", "", regex=True)
+        .astype(float)
+    )
+
+    # Limpieza de fecha_de_beneficio
+    df["fecha_de_beneficio"] = pd.to_datetime(
+        df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+    ).fillna(
+        pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce")
+    )
+
+    # Cambio de tipo de dato de comuna_ciudadano
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+
+    # Eliminación de duplicados y valores nulos
+    df = df.drop_duplicates().dropna()
+
     return df
 
-df = clean_data()
-print(df.línea_credito.value_counts().to_list())
+
+clean_data()
